3f571835f009a5367b551ae06c617c8a4e846164
FSZ560 <gray930522@gmail.com>
2024-12-07 23:47:07 +0800

    update database schema

--- sql/db_team_11_project.sql
+++ sql/db_team_11_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2024-12-07 09:41:20
+-- 產生時間： 2024-12-07 16:45:08
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -24,6 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `appointment_id` char(7) NOT NULL,
+  `sequence_number` int(10) UNSIGNED NOT NULL,
+  `clinic_id` char(7) NOT NULL,
+  `patient_id` char(7) NOT NULL,
+  `register_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `clinic`
+--
+
+CREATE TABLE `clinic` (
+  `clinic_id` char(7) NOT NULL,
+  `clinic_date` date NOT NULL,
+  `period` enum('morning','afternoon','evening') NOT NULL,
+  `department_id` char(7) NOT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `doctor_id` char(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- 傾印資料表的資料 `clinic`
+--
+
+INSERT INTO `clinic` (`clinic_id`, `clinic_date`, `period`, `department_id`, `location`, `doctor_id`) VALUES
+('CL00001', '2024-12-07', 'morning', 'DE00001', '第一醫療大樓205室', 'PE00002');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `department`
 --
 
@@ -37,17 +73,199 @@ CREATE TABLE `department` (
 --
 
 INSERT INTO `department` (`department_id`, `department_name`) VALUES
-('0000001', '眼科');
+('DE00001', '眼科'),
+('DE00002', '家醫科');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `medical_certificate`
+--
+
+CREATE TABLE `medical_certificate` (
+  `certificate_id` char(7) NOT NULL,
+  `record_id` char(7) NOT NULL,
+  `prescription` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `medical_record`
+--
+
+CREATE TABLE `medical_record` (
+  `record_id` char(7) NOT NULL,
+  `patient_id` char(7) NOT NULL,
+  `clinic_id` char(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `patient`
+--
+
+CREATE TABLE `patient` (
+  `person_id` char(7) NOT NULL,
+  `height` decimal(5,2) DEFAULT NULL,
+  `weight` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- 傾印資料表的資料 `patient`
+--
+
+INSERT INTO `patient` (`person_id`, `height`, `weight`) VALUES
+('PE00001', 162.00, 50.00);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `person`
+--
+
+CREATE TABLE `person` (
+  `person_id` char(7) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `id_card` char(10) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `gender` enum('M','F') DEFAULT NULL,
+  `birthday` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- 傾印資料表的資料 `person`
+--
+
+INSERT INTO `person` (`person_id`, `last_name`, `first_name`, `id_card`, `phone`, `address`, `gender`, `birthday`) VALUES
+('PE00001', '長崎', '爽世', 'A123456789', '0912345678', NULL, 'F', '2008-05-27'),
+('PE00002', '千早', '愛音', 'A164551397', NULL, NULL, 'F', '2008-09-08');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `staff`
+--
+
+CREATE TABLE `staff` (
+  `person_id` char(7) NOT NULL,
+  `department_id` char(7) NOT NULL,
+  `salary` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- 傾印資料表的資料 `staff`
+--
+
+INSERT INTO `staff` (`person_id`, `department_id`, `salary`) VALUES
+('PE00002', 'DE00001', NULL);
 
 --
 -- 已傾印資料表的索引
 --
 
 --
+-- 資料表索引 `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appointment_id`),
+  ADD KEY `clinic_id` (`clinic_id`),
+  ADD KEY `patient_id` (`patient_id`);
+
+--
+-- 資料表索引 `clinic`
+--
+ALTER TABLE `clinic`
+  ADD PRIMARY KEY (`clinic_id`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `doctor_id` (`doctor_id`);
+
+--
 -- 資料表索引 `department`
 --
 ALTER TABLE `department`
   ADD PRIMARY KEY (`department_id`);
+
+--
+-- 資料表索引 `medical_certificate`
+--
+ALTER TABLE `medical_certificate`
+  ADD PRIMARY KEY (`certificate_id`),
+  ADD KEY `record_id` (`record_id`);
+
+--
+-- 資料表索引 `medical_record`
+--
+ALTER TABLE `medical_record`
+  ADD PRIMARY KEY (`record_id`),
+  ADD KEY `patient_id` (`patient_id`),
+  ADD KEY `clinic_id` (`clinic_id`);
+
+--
+-- 資料表索引 `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`person_id`);
+
+--
+-- 資料表索引 `person`
+--
+ALTER TABLE `person`
+  ADD PRIMARY KEY (`person_id`);
+
+--
+-- 資料表索引 `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`person_id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- 已傾印資料表的限制式
+--
+
+--
+-- 資料表的限制式 `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`clinic_id`) REFERENCES `clinic` (`clinic_id`),
+  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`person_id`);
+
+--
+-- 資料表的限制式 `clinic`
+--
+ALTER TABLE `clinic`
+  ADD CONSTRAINT `clinic_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`),
+  ADD CONSTRAINT `clinic_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `staff` (`person_id`);
+
+--
+-- 資料表的限制式 `medical_certificate`
+--
+ALTER TABLE `medical_certificate`
+  ADD CONSTRAINT `medical_certificate_ibfk_1` FOREIGN KEY (`record_id`) REFERENCES `medical_record` (`record_id`);
+
+--
+-- 資料表的限制式 `medical_record`
+--
+ALTER TABLE `medical_record`
+  ADD CONSTRAINT `medical_record_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`person_id`),
+  ADD CONSTRAINT `medical_record_ibfk_2` FOREIGN KEY (`clinic_id`) REFERENCES `clinic` (`clinic_id`);
+
+--
+-- 資料表的限制式 `patient`
+--
+ALTER TABLE `patient`
+  ADD CONSTRAINT `patient_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
+
+--
+-- 資料表的限制式 `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`),
+  ADD CONSTRAINT `staff_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
